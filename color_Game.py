3c7e0a8c5b4c8390317560405572c0a452e90734
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-20 23:02:17 +0530

    color name label

--- color_Game.py
+++ color_Game.py
@@ -10,6 +10,9 @@ root.title('Color Game')
 # set the window size
 root.geometry('700x500')
 
+# background color of the window
+root.config(bg="#d6ccc2")
+
 # Instruction Label
 instruction = Label(root, text="Type the color of the words.",
                     font=("Helvetica", 20, 'bold'))
@@ -24,6 +27,10 @@ score.place(x=250, y=70, width=200)
 time = Label(root, text='Time Left: ',
              font=("Helvetica", 15, "bold"))
 time.place(x=250, y=100, width=200)
+
+# Label for displaying the colors name
+colors_name = Label(root, font=('Helvetica', 20, 'bold'))
+colors_name.place(x=200, y=150, width=400, height=100)
 
 
 # start the GUI
