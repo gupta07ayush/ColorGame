bd2cb4303cd89895decc35bb72c93988917994fd
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-20 21:58:04 +0530

    start the GUI

---
+++ color_Game.py
@@ -0,0 +1,15 @@
+import tkinter
+import random
+
+# create tkinter GUI
+root = tkinter.Tk()
+
+# set the title of window
+root.title('Color Game')
+
+# set the window size
+root.geometry('700x500')
+
+
+# start the GUI
+root.mainloop()
