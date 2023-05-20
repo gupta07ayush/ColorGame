fa6447ea84da309f617635d44def7eb9a3c959b7
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-20 22:09:46 +0530

    instruction Label

--- color_Game.py
+++ color_Game.py
@@ -1,14 +1,19 @@
-import tkinter
+from tkinter import Tk, Label, Entry, Button
 import random
 
 # create tkinter GUI
-root = tkinter.Tk()
+root = Tk()
 
 # set the title of window
 root.title('Color Game')
 
 # set the window size
 root.geometry('700x500')
+
+# Add an Instruction Label
+instruction = Label(root, text="Type the color of the words.",
+                    font=("Helvetica", 20, 'bold'))
+instruction.place(x=150, y=30, width=400)
 
 
 # start the GUI
