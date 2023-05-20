f1d549a04ea702f9935277756b62c234bad7f296
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-20 22:17:03 +0530

    Time Left

--- color_Game.py
+++ color_Game.py
@@ -16,9 +16,14 @@ instruction = Label(root, text="Type the color of the words.",
 instruction.place(x=150, y=30, width=400)
 
 # Score Label
-score = Label(root, text='Press Enter to start',
+score = Label(root, text='Press Enter to Start',
               font=("Helvetica", 15, "bold"))
 score.place(x=250, y=70, width=200)
+
+# Time left label
+time = Label(root, text='Time Left: ',
+             font=("Helvetica", 15, "bold"))
+time.place(x=250, y=100, width=200)
 
 
 # start the GUI
